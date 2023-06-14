02a223b8192bc5b01a5ee6fea0bda52e7f567111
Shivansh1357 <120310888+Shivansh1357@users.noreply.github.com>
2023-06-14 13:15:41 +0530

    submitting code-Wed Jun 14 2023 13:15:34 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,14 @@ import './../styles/App.css';
 import Button from  "./Button"
 
 const App = () => {
+  let [count,setCount]=useState(0);
+  function countClicks(){
+    setCount(count+1)
+  }
   return (
     <div>
-       <Button/>
+      <p> Button clicked {cnt} times</p>
+      <button onClick={countClicks}>Click me</button>
     </div>
   )
 }
